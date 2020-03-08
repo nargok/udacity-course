09987f93e34dc4a19b08a698fb383796cae8967c
nargok <nargok@gmail.com>
2020-03-08 15:57:14 +0900

    category icon

--- course/02_category_widget/task_02_category_widget/lib/category.dart
+++ course/02_category_widget/task_02_category_widget/lib/category.dart
@@ -6,6 +6,9 @@
 // https://www.dartlang.org/guides/language/effective-dart/style#ordering
 import 'package:flutter/material.dart';
 
+final _height = 100.0;
+final _borderRadius = BorderRadius.circular(_height / 2);
+
 /// A custom [Category] widget.
 ///
 /// The widget is composed on an [Icon] and [Text]. Tapping on the widget shows
@@ -16,7 +19,11 @@ class Category extends StatelessWidget {
   /// A [Category] saves the name of the Category (e.g. 'Length'), its color for
   /// the UI, and the icon that represents it (e.g. a ruler).
   // TODO: You'll need the name, color, and iconLocation from main.dart
-  const Category();
+  final IconData icon;
+  final String iconText;
+  final Color iconColor;
+  const Category(this.icon, this.iconText, this.iconColor);
+
 
   /// Builds a custom widget that shows [Category] information.
   ///
@@ -28,6 +35,37 @@ class Category extends StatelessWidget {
   // See https://docs.flutter.io/flutter/material/Theme-class.html
   Widget build(BuildContext context) {
     // TODO: Build the custom widget here, referring to the Specs.
-    return Container();
+    return Container(
+      height: _height, // todo make it constants
+//      decoration:
+//          new BoxDecoration(borderRadius: new BorderRadius.circular(50.0)),
+      child: Padding(
+          padding: EdgeInsets.all(8.0), //
+          child: InkWell(
+            borderRadius: _borderRadius,
+            highlightColor: this.iconColor,
+            splashColor: this.iconColor,
+            onTap: () {
+              print('I was tapped!');
+            },
+            child: Row(
+              children: <Widget>[
+                Padding(
+                  padding: EdgeInsets.all(8),
+                  child: Icon(
+                    this.icon, // todo パラメータ渡し
+                    size: 60.0,
+                  ),
+                ),
+                Center(
+                  child: Text(
+                    this.iconText, // todo パラメータ渡し
+                    style: TextStyle(fontSize: 24.0),
+                  ),
+                )
+              ],
+            ),
+          )),
+    );
   }
 }
